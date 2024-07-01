16fa44c758ce2b32d44a337f58711a32e8a7f3bf
Sanjay D C <sanjayc1@vmware.com>
2024-07-01 08:41:02 +0000

    PROT-594 Remove stale namespaces

--- ci/scripts/vsphere-automation/cleanup_stale_namespaces.sh
+++ ci/scripts/vsphere-automation/cleanup_stale_namespaces.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set +x
 # Delete all the namespaces which were created by CI pipeline
-kubectl get ns -o go-template --template '{{range .items}}{{.metadata.name}} {{.metadata.creationTimestamp}}{{"\n"}}{{end}}' | \
-    awk '$2 <= "'$(date -d '24 hour ago' -Ins --utc | \
-    sed 's/+0000/Z/')'" { print $1 }' | \
-    { grep ci- || true; } | \
+kubectl get ns -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' | { grep ci- || true; } | \
     xargs -I {} --no-run-if-empty python3 ci/scripts/vsphere-automation/client.py -c namespace -o delete -ns "{}"
